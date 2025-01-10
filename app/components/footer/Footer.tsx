e1c514e9502845af3919c0ac44a7dcb3006658a7
Bathsheba <bethkerubo193@gmail.com>
2025-01-10 17:15:05 +0300

    Initial commit

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,82 @@
+import Link from "next/link";
+import Container from "../Contaner";
+import FooterList from "./FooterList";
+import { MdFacebook} from "react-icons/md";
+import{
+    AiFillTwitterCircle,
+    AiFillInstagram,
+    AiFillYoutube
+} from "react-icons/ai"
+
+const Footer = () => {
+    return ( <footer className="
+        bg-slate-700
+        text-slate-200
+        text-sm
+        mt-16">
+            <Container>
+                <div className="
+                flex
+                flex-col
+                md:flex-row
+                justify-between
+                pt-16
+                pb-8">
+                    <FooterList>
+                        <h3 className="text-base font-bold mb-2">Shop Categories</h3>
+                        <Link href="#">Phones </Link>
+                        <Link href="#"> Laptops</Link>
+                        <Link href="#"> Desktops</Link>
+                        <Link href="#"> Watches</Link>
+                        <Link href="#"> TVs</Link>
+                        <Link href="#"> Accessories </Link>
+                    </FooterList>
+                    <FooterList>
+                        <h3 className="text-base font-bold mb-2">Customer Services</h3>
+                        <Link href="#">Contact Us </Link>
+                        <Link href="#"> Shipping Policy</Link>
+                        <Link href="#"> Returns & Exchanges</Link>
+                        <Link href="#"> FAQs</Link>
+                    </FooterList>
+                    <div className="w-full md:w-1/3 mb-6 md:mb-0 ">
+                        <h3 className="text-base font-bold mb-2">About Us</h3>
+                        <p className="mb-2">
+                        As a leading electronics retailer, 
+                        we offer a wide range of products, 
+                        from the latest smartphones and laptops to home theater 
+                        systems and smart home devices. 
+                        We are committed to staying ahead of the curve and 
+                        bringing you the most innovative and exciting tech on the market. 
+                        </p>
+                        <p>&copy;  {new Date().getFullYear()} E-Shop. All rights reserved</p>
+
+                    </div>
+
+                    <FooterList>
+                        <h3 className="text-base font-bold mb-2">Follow Us</h3>
+                        <div className="flex gap-2">
+                            <Link href="/">
+                            <MdFacebook size={24} />
+                            </Link>
+
+                            <Link href="/">
+                            <AiFillTwitterCircle size={24} />
+                            </Link>
+
+                            <Link href="/">
+                            <AiFillInstagram size={24} />
+                            </Link>
+
+                            <Link href="/">
+                            <AiFillYoutube size={24} />
+                            </Link>
+
+                        </div>
+
+                    </FooterList>
+                </div>
+            </Container>
+        </footer> );
+};
+ 
+export default Footer;
